3fb33a09abd006c76672aa3492c59cc30f0a1d08
Niklas Zeidler <github@niklas.firlefanz.org>
2021-12-02 20:48:19 +0100

    solved both parts for day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -20,7 +20,7 @@ fn task1() {
             }
         }
     }
-    println!("{}",horizontal * depth);
+    println!("Result from part 1 is: {}",horizontal * depth);
 }
 
 fn task2() {
@@ -37,18 +37,20 @@ fn task2() {
             let units: i32 = split.next().unwrap().parse().ok().unwrap_or(0);
             //current = line.parse().unwrap();
             match instruction {
-                "forward" => horizontal = horizontal + units,
-                "down" => depth = depth + units,
-                "up" => depth = depth - units, 
+                "forward" => {horizontal = horizontal + units;
+                depth = depth + aim*units;},
+                "down" => aim = aim + units,
+                "up" => aim = aim - units, 
                 _ => println!("could also be empty"),
 
             }
         }
     }
-    println!("{}",horizontal * depth);
+    println!("Result from part 2 is: {}",horizontal * depth);
 }
 
 
 fn main(){
+    task1();
     task2();
 }
--- day2/target/debug/day2.d
+++ day2/target/debug/day2.d
@@ -1,1 +1,1 @@
-C:\Users\NiklasZeidler\source\repos\AdventOfCode2021\day2\target\debug\day2.exe: C:\Users\NiklasZeidler\source\repos\AdventOfCode2021\day2\src\main.rs
+/home/nik/Coding/AdventOfCode2021/day2/target/debug/day2: /home/nik/Coding/AdventOfCode2021/day2/src/main.rs
