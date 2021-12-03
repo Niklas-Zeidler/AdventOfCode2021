a82044701d2a1dd148d7171b1709ce1bc42c799a
Niklas Zeidler <github@niklas.firlefanz.com>
2021-12-03 22:47:40 +0100

    further along in task2 day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -25,8 +25,6 @@ fn task1() {
         if *bit != 0 {
             sign = bit / i32::abs(*bit);
         }
-        println!("i is: {} and sign is: {}",i,sign);        
-        
         if sign == 1 { 
            gamma =  two.pow(i.try_into().unwrap()) + gamma;
         }
@@ -45,10 +43,103 @@ fn task1() {
 }
 
 fn task2() {
+    // check with sample input
+    let contents = fs::read_to_string("sample.txt")
+        .expect("Something went wrong reading the file");
+
+    let mut array = vec![0;contents.len()];
+    let size : usize = 5;
+    for (i, line) in contents.lines().enumerate(){
+        let temp = u32::from_str_radix(line, 2).unwrap();
+        array[i] = temp as u32;
+    }
+    let  best_epsilon : u32 = search_for_closest_element(array, size,true);
+    // let  best_gamma : u32 = search_for_closest_element(array.to_vec(), size,false);
+    // println!("oxygen generator rating: {}", best_epsilon);
+    // println!("co2 scrubber rating : {}", best_gamma);
+    // println!("solution task2 : {}", best_gamma * best_epsilon);
+
+    // 
+    // let  best_epsilon : u32 = search_for_closest_element(array.to_vec(), epsilon, size,true);
+    // let  best_gamma : u32 = search_for_closest_element(array,gamma,size,false);
+
+    // let gamma = 784;
+    // let epsilon = 3311;
+    // let contents = fs::read_to_string("input.txt")
+    //     .expect("Something went wrong reading the file");
+
+    // let mut array = vec![0;contents.len()];
+    // let size : usize = 12;
+    // for (i, line) in contents.lines().enumerate(){
+    //     let temp = u32::from_str_radix(line, 2).unwrap();
+    //     array[i] = temp as u32;
+    // }
+    // let  best_epsilon : u32 = search_for_closest_element(array.to_vec(), epsilon, size,true);
+    // let  best_gamma : u32 = search_for_closest_element(array,gamma,size,false);
+    
+    // println!("oxygen generator rating: {}", best_epsilon);
+    // println!("co2 scrubber rating : {}", best_gamma);
+    // println!("solution task2 : {}", best_gamma * best_epsilon);
+
+    
 }
 
+fn search_for_closest_element(mut array : Vec<u32>,bit_range:usize,modifier : bool)->u32{
+    let two : u32 = 2;
+    let mut more_than_one_number = true;
+    let mut comparing_value :u32;
+    while more_than_one_number {
+        for shift in (0..bit_range).rev() {
+            let bit_selector =  two.pow(shift as u32);
+            let mut sum = 0;
+
+            for remaining in array.iter(){
+                sum = (remaining & bit_selector) + sum;
+                println!("{}",remaining);
+            }
+            comparing_value = 0;
+            if modifier {
+                if ((sum as f32) / (array.len() as f32 * bit_selector as f32)) >= 0.5 { comparing_value = bit_selector;}
+            }
+            else{
+                if ((sum as f32) / (array.len() as f32 * bit_selector as f32)) > 0.5 { comparing_value = bit_selector;}
+            }
+            let old_array = array.to_vec();
+            array.retain(|&x| !((x & bit_selector) > 0)^((comparing_value & bit_selector) > 0));
+            // if shift < two.pow(3).try_into().unwrap()  {println!("{:?}", array);}
+            // println!("shift : {}, comparing value: {}, length : {}",shift,comparing_value, array.len());
+            if array.len() == 0 {
+                array = old_array;
+                more_than_one_number = false;
+            }
+            if shift == (bit_range-1){
+                more_than_one_number = false;
+            }
+            if array.len() == 1 {
+                more_than_one_number = false;
+                println!("{:?}", array);
+                break;
+            }
+
+        }
+    }
+    // let return_value : u32;
+    // if array.len() != 1{
+    //     if modifier {
+    //         return_value = *array.iter().max().unwrap();
+    //     }
+    //     else{
+    //         return_value = *array.iter().min().unwrap();
+    //     }
+    // }
+    // else{
+    //     return_value = array[0];
+    // }
+    array[0]
+    // return_value
+}
 
 fn main(){
     task1();
-//    task2();
+    task2();
 }
